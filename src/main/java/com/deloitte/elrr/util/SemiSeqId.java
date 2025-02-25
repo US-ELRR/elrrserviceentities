f2808430ccafe22c2471073158cb7e1a94007890
Cliff Casey <cliff@yetanalytics.com>
2025-02-25 14:53:19 -0500

    deps rearrange and upgrade to seqid gen

---
+++ src/main/java/com/deloitte/elrr/util/SemiSeqId.java
@@ -0,0 +1,16 @@
+package com.deloitte.elrr.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
+import org.hibernate.annotations.IdGeneratorType;
+
+@IdGeneratorType(SemiSeqUUIdGenerator.class)
+@Retention( RetentionPolicy.RUNTIME)
+@Target({ FIELD, METHOD})
+public @interface SemiSeqId {
+
+}
