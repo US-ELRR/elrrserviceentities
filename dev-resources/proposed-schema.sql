c824efd32107d22035ef35b2e9ad82ae3b0c23cd
Leven <phleven@deloitte.com>
2025-03-05 14:38:02 -0500

    Remove ELRRAuditLog table.

--- dev-resources/proposed-schema.sql
+++ dev-resources/proposed-schema.sql
@@ -311,24 +311,3 @@ CREATE TABLE IF NOT EXISTS military_record (
     inserted_date               TIMESTAMP WITH TIME ZONE,
     last_modified               TIMESTAMP WITH TIME ZONE
 );
-
--- Create elrr tables 
-CREATE TABLE IF NOT EXISTS elrr.elrrauditlog (
-    elrrauditlogid int8 NOT NULL,
-    syncid int8 NOT NULL,
-    recordstatus varchar(10) NULL, 
-    updatedby varchar(20) NULL,
-    inserteddate timestamp NULL,
-    lastmodified timestamp NULL,
-    CONSTRAINT elrrauditlog_pk PRIMARY KEY (elrrauditlogid)
-);
-
-CREATE SEQUENCE IF NOT EXISTS elrr.elrrauditlog_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-    NO CYCLE;
-
-ALTER SEQUENCE elrr.elrrauditlog_seq OWNED BY elrr.elrrauditlog.elrrauditlogid;
--- src/main/java/com/deloitte/elrr/repository/ELRRAuditLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.deloitte.elrr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.deloitte.elrr.entity.ELRRAuditLog;
-
-@Repository
-public interface ELRRAuditLogRepository extends JpaRepository<ELRRAuditLog, Long> {}
