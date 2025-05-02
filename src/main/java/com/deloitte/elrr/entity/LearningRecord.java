ad93b8b6284fc0b1a9eef8de333625647d520af7
Leven <phleven@deloitte.com>
2025-05-02 10:25:53 -0400

    Repalce tabs with white space.

--- src/main/java/com/deloitte/elrr/entity/LearningRecord.java
+++ src/main/java/com/deloitte/elrr/entity/LearningRecord.java
@@ -28,44 +28,32 @@ import lombok.Setter;
 @Setter
 public class LearningRecord extends Auditable<String> {
 
-  @ManyToOne
-  @JoinColumn(name = "learning_resource_id")
-  private LearningResource learningResource;
+    @ManyToOne
+    @JoinColumn(name = "learning_resource_id")
+    private LearningResource learningResource;
 
-  @ManyToOne
-  @JoinColumn(name = "person_id")
-  private Person person;
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
 
-  @Column(name = "enrollment_date")
-  private LocalDate enrollmentDate;
+    @Column(name = "enrollment_date")
+    private LocalDate enrollmentDate;
 
-  @Column(name = "record_status", columnDefinition = "elrr.learning_status")
-  @Enumerated(EnumType.STRING)
-  @JdbcType(PostgreSQLEnumJdbcType.class)
-  private LearningStatus recordStatus;
+    @Column(name = "record_status", columnDefinition = "elrr.learning_status")
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    private LearningStatus recordStatus;
 
-  @Column(name = "academic_grade", length = 50)
-  private String academicGrade;
+    @Column(name = "academic_grade", length = 50)
+    private String academicGrade;
 
-  @Column(name = "event_time")
-  private LocalDateTime eventTime;
+    @Column(name = "event_time")
+    private LocalDateTime eventTime;
 
-  @Override
-  public String toString() {
-    return "LearningRecord [id="
-        + id
-        + ", learningResource="
-        + learningResource
-        + ", person="
-        + person
-        + ", enrollmentDate="
-        + enrollmentDate
-        + ", recordStatus="
-        + recordStatus
-        + ", academicGrade="
-        + academicGrade
-        + ", eventTime="
-        + eventTime
-        + "]";
-  }
+    @Override
+    public String toString() {
+        return "LearningRecord [id=" + id + ", learningResource=" + learningResource + ", person=" + person
+                + ", enrollmentDate=" + enrollmentDate + ", recordStatus=" + recordStatus + ", academicGrade="
+                + academicGrade + ", eventTime=" + eventTime + "]";
+    }
 }
