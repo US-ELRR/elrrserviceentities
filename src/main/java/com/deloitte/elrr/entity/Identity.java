8d05878603301dc3c76aadfb2cf184548fa4ac95
Leven <phleven@deloitte.com>
2025-05-13 17:42:54 -0400

    Identity.java.

--- src/main/java/com/deloitte/elrr/entity/Identity.java
+++ src/main/java/com/deloitte/elrr/entity/Identity.java
@@ -39,58 +39,52 @@ public class Identity extends Auditable<String> {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "ifi", nullable = false, length = 255, updatable = false,
-        insertable = false)
+    @Column(name = "ifi", nullable = false, length = 255, updatable = false, insertable = false)
     @Setter(AccessLevel.NONE)
     @Getter(AccessLevel.NONE)
     private String ifi;
 
     /**
-    * Static method to return an xAPI Inverse Functional Identifier for actor
-    * properties.
-    * Should work for any incoming actor to help lookup existing identities.
-    *
-    * @param  mboxSha1Sum  mboxsha1sum property of actor
-    * @param  mbox         mbox property of actor
-    * @param  openid       openid property of actor
-    * @param  homePage     account.homePage property of actor
-    * @param  name         account.name property of actor
-    * @return              the Inverse Functional Identifier
-    */
+     * Static method to return an xAPI Inverse Functional Identifier for actor
+     * properties. Should work for any incoming actor to help lookup existing
+     * identities.
+     *
+     * @param mboxSha1Sum mboxsha1sum property of actor
+     * @param mbox2       mbox property of actor
+     * @param openid2     openid property of actor
+     * @param homePage    account.homePage property of actor
+     * @param name        account.name property of actor
+     * @return the Inverse Functional Identifier
+     */
     @Transient
-    public static String createIfi(String mboxSha1Sum, String mbox,
-                    String openid, String homePage, String name) {
+    public static String createIfi(String mboxSha1Sum, String mbox2, String openid2, String homePage, String name) {
         final String ifiTemplate = "%s::%s";
         final String accountTemplate = "%s@%s";
         if (mboxSha1Sum != null) {
             return String.format(ifiTemplate, "mbox_sha1sum", mboxSha1Sum);
-        } else if (openid != null) {
-            return String.format(ifiTemplate, "openid", openid);
+        } else if (openid2 != null) {
+            return String.format(ifiTemplate, "openid", openid2);
         } else if (homePage != null) {
-            return String.format(ifiTemplate, "account",
-                    String.format(accountTemplate, name, homePage));
+            return String.format(ifiTemplate, "account", String.format(accountTemplate, name, homePage));
         } else {
-            return String.format(ifiTemplate, "mbox", mbox);
+            return String.format(ifiTemplate, "mbox", mbox2);
         }
     }
 
     /**
-    * Uses createIfi to form an IFI from existing Identity properties.
-    *
-    * @return the current Inverse Functional Identifier
-    */
+     * Uses createIfi to form an IFI from existing Identity properties.
+     *
+     * @return the current Inverse Functional Identifier
+     */
     public String getIfi() {
         return createIfi(mboxSha1Sum, mbox, openid, homePage, name);
     }
 
     @Override
     public String toString() {
-        String personValue =  (person != null)
-            ? person.getId().toString() : null;
-        return "Identity [person=" + personValue + ", mboxSha1Sum="
-        + mboxSha1Sum + ", id=" + id + ", mbox=" + mbox + ", openid=" + openid
-        + ", homePage=" + homePage + ", name = " + name + ", ifi=" + getIfi()
-        + "]";
+        String personValue = (person != null) ? person.getId().toString() : null;
+        return "Identity [person=" + personValue + ", mboxSha1Sum=" + mboxSha1Sum + ", id=" + id + ", mbox=" + mbox
+                + ", openid=" + openid + ", homePage=" + homePage + ", name = " + name + ", ifi=" + getIfi() + "]";
     }
 
 }
