df8473dbb811304d49f2eace214295f58202b8dd
Milton Reder <milt@yetanalytics.com>
2025-06-18 17:25:56 -0400

    remove inheritance so we can set an id

--- src/main/java/com/deloitte/elrr/entity/ClientToken.java
+++ src/main/java/com/deloitte/elrr/entity/ClientToken.java
@@ -2,27 +2,52 @@ package com.deloitte.elrr.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.UUID;
 
 @Entity
 @Table(name = "client_token")
+@EntityListeners(AuditingEntityListener.class)
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class ClientToken extends Auditable<String> {
+public class ClientToken {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
 
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(name = "jwtPayload", columnDefinition = "JSONB")
     private Map<String, Object> jwtPayload;
+
+    @Column(name = "inserted_date", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime insertedDate;
+
+    @Column(name = "updated_by", length = 20)
+    @LastModifiedBy
+    private String updatedBy;
+
+    @Column(name = "last_modified")
+    @UpdateTimestamp
+    private LocalDateTime lastModified;
 
     @Override
     public String toString() {
