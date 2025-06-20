feae1e20288747716bf916b82cf6d719cacfb2ae
Milton Reder <milt@yetanalytics.com>
2025-06-20 12:11:09 -0400

    let ID gen preserve entity ID

--- src/main/java/com/deloitte/elrr/util/SemiSeqUUIdGenerator.java
+++ src/main/java/com/deloitte/elrr/util/SemiSeqUUIdGenerator.java
@@ -5,11 +5,21 @@ import org.hibernate.id.IdentifierGenerator;
 
 import com.fasterxml.uuid.Generators;
 
+import com.deloitte.elrr.entity.Entity;
+
 public class SemiSeqUUIdGenerator implements IdentifierGenerator {
 
     @Override
     public Object generate(final SharedSessionContractImplementor session,
                     final Object object) {
+        // do a checked cast on Object to Entity and check if it already
+        // has an ID. If it does, return that ID
+        if (object instanceof Entity) {
+            Entity entity = (Entity) object;
+            if (entity.getId() != null) {
+                return entity.getId();
+            }
+        }
         return Generators.timeBasedEpochRandomGenerator().generate();
     }
 }
