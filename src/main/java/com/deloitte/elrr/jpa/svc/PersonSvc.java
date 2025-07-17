98593835392a60dc4fda263f5452a74497c8347f
Milton Reder <milt@yetanalytics.com>
2025-07-17 10:56:36 -0400

    undo autoformat stupids

--- src/main/java/com/deloitte/elrr/jpa/svc/PersonSvc.java
+++ src/main/java/com/deloitte/elrr/jpa/svc/PersonSvc.java
@@ -20,7 +20,8 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 
-@Service @Slf4j
+@Service
+@Slf4j
 public class PersonSvc implements CommonSvc<Person, UUID> {
 
     private final PersonRepository personRepository;
