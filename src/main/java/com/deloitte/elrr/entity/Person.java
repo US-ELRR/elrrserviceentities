69ecb69ecc948b7684d5151b9c4fb95804aa4789
Milton Reder <milt@yetanalytics.com>
2025-07-18 11:17:52 -0400

    outdent query because json function names get wide

--- src/main/java/com/deloitte/elrr/entity/Person.java
+++ src/main/java/com/deloitte/elrr/entity/Person.java
@@ -23,20 +23,21 @@ import lombok.Setter;
 @Table(name = "person")
 @NamedQuery(
     name = "Person.findPersonsWithFilters",
-    query = """
-            SELECT DISTINCT p FROM Person p
-            LEFT JOIN p.identities i
-            LEFT JOIN p.associations a
-            LEFT JOIN p.employmentRecords er
-            WHERE (:id IS NULL OR p.id = :id)
-            AND (:ifi IS NULL OR i.ifi = :ifi)
-            AND (:organizationId IS NULL OR
-                ((:organizationRelType IS NULL OR
-                  :organizationRelType = 'Association')
-                 AND a.organization.id = :organizationId)
-                OR (:organizationRelType = 'Employment'
-                    AND er.employerOrganization.id = :organizationId))
-            """
+    query =
+    """
+    SELECT DISTINCT p FROM Person p
+    LEFT JOIN p.identities i
+    LEFT JOIN p.associations a
+    LEFT JOIN p.employmentRecords er
+    WHERE (:id IS NULL OR p.id = :id)
+    AND (:ifi IS NULL OR i.ifi = :ifi)
+    AND (:organizationId IS NULL OR
+        ((:organizationRelType IS NULL OR
+          :organizationRelType = 'Association')
+         AND a.organization.id = :organizationId)
+        OR (:organizationRelType = 'Employment'
+            AND er.employerOrganization.id = :organizationId))
+    """
 )
 @RequiredArgsConstructor
 @AllArgsConstructor
