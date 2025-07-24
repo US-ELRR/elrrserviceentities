94cceeae155522be9f9834adff8f6dba15625f49
Milton Reder <milt@yetanalytics.com>
2025-07-24 11:15:06 -0400

    comment up query since it's getting big

--- src/main/java/com/deloitte/elrr/entity/Person.java
+++ src/main/java/com/deloitte/elrr/entity/Person.java
@@ -33,14 +33,18 @@ import lombok.Setter;
         ON a.organization_id = org_assoc.id
     LEFT JOIN {h-schema}organization org_emp
         ON er.employer_organization = org_emp.id
+    -- by ID, TODO: make multiple IDs work
     WHERE (CAST(:id AS uuid) IS NULL OR p.id = :id)
+    -- by IFI, TODO: make multiple IFIs work
     AND (CAST(:ifi AS text) IS NULL OR i.ifi = :ifi)
+    -- by organization, via association or employment
     AND (CAST(:organizationId AS uuid) IS NULL OR
         ((CAST(:organizationRelType AS text) IS NULL
             OR :organizationRelType = 'Association')
          AND org_assoc.id = :organizationId)
         OR (:organizationRelType = 'Employment'
             AND org_emp.id = :organizationId))
+    -- by presence of (all) extension keys
     AND (CAST(:hasExtension AS text[]) IS NULL OR
         p.extensions \\?\\?& CAST(:hasExtension AS text[]))
     """,
