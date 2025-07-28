e4a61bda877e5192f40c32f502a8c5236c10d452
Leven <phleven@deloitte.com>
2025-07-28 15:47:17 -0400

    Fix comments.

--- src/main/java/com/deloitte/elrr/jpa/svc/GoalSvc.java
+++ src/main/java/com/deloitte/elrr/jpa/svc/GoalSvc.java
@@ -79,7 +79,7 @@ public class GoalSvc implements CommonSvc<Goal, UUID> {
     /**
      * Sets the credentials for the goal from an array of credential IDs.
      *
-     * @param goal          The goal to set credentials for.
+     * @param goal The goal to set credentials for.
      * @param credentialIds The array of credential IDs.
      * @return The updated goal with credentials set.
      */
@@ -102,7 +102,7 @@ public class GoalSvc implements CommonSvc<Goal, UUID> {
      * Sets the learning resources for the goal from an array of learning
      * resource IDs.
      *
-     * @param goal                The goal to set learning resources for.
+     * @param goal The goal to set learning resources for.
      * @param learningResourceIds The array of learning resource IDs.
      * @return The updated goal with learning resources set.
      */
